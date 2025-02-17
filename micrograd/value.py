dcc4ab0439112a8e3928d877c1079b6662cfcaa8
zzwhoops-home <zd@5rock.org>
2025-02-17 16:33:54 -0500

    Initial commit

---
+++ micrograd/value.py
@@ -0,0 +1,12 @@
+class Value:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
