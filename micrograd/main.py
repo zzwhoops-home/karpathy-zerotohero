0abcd0012517e5ae05c010b35f9ade41417bb0d9
zzwhoops-home <zd@5rock.org>
2025-02-17 16:47:37 -0500

    add node graph visualizer

--- micrograd/main.py
+++ micrograd/main.py
@@ -20,7 +20,7 @@ def draw_dot(root):
   for n in nodes:
     uid = str(id(n))
     # for any value in the graph, create a rectangular ('record') node for it
-    dot.node(name = uid, label = "{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
+    dot.node(name = uid, label = "{ %s | data %.4f }" % (n.label, n.data), shape='record')
     if n._op:
       # if this value is a result of some operation, create an op node for it
       dot.node(name = uid + n._op, label = n._op)
