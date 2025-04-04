e5a941e85e02904f2fedda123e2ae57bf0490981
zzwhoops-home <zd@5rock.org>
2025-04-03 20:07:10 -0400

    one step backpropagation and optimization notes

--- micrograd/main.py
+++ micrograd/main.py
@@ -50,15 +50,20 @@ def lol():
     # added to a
     a = Value(2.0, label='a')
     b = Value(-3.0, label='b')
+    b.data += h
     c = Value(10.0, label='c')
     # d = a * b + c; d.label = 'd' # creates d with -6, which is passed as child with the value of c to children
     e = a * b; e.label = 'e'
     d = e + c; d.label = 'd'
     f = Value(-2.0, label='f')
     L = d * f; L.label = 'L'
-    L2 = L.data + h
+    L2 = L.data
 
+    # change in L, normalize by H (rise / run)
+    # deriv of L w/ respect to h
     print((L2 - L1) / h)
+
+    L.grad = (L2 - L1) / h
 
 def draw():
     a = Value(2.0, label='a')
@@ -69,9 +74,43 @@ def draw():
     d = e + c; d.label = 'd'
     f = Value(-2.0, label='f')
     L = d * f; L.label = 'L'; L.grad = 1.0
+    
+    f.grad = 4.0
+    d.grad = -2.0
+    c.grad = -2.0
+    e.grad = -2.0
+    b.grad = -4.0
+    a.grad = 6.0
+
+    # one step of an optimization, hyperparameter alpha
+    alpha = 0.01
+    a.data += alpha * a.grad
+    b.data += alpha * b.grad
+    c.data += alpha * c.grad
+    f.data += alpha * f.grad
+
+    e = a * b
+    d = e + c
+    L = d * f
+    print(L.data)
     draw_dot(L).render()
 
 if __name__ == "__main__":
     # manual backprop: how much does L change with a small change h?
     lol()
     draw()
+
+    # back a step:
+    # what is dL/dd? (propagating backwards)
+    # dL / dd = f
+    # L = d * f, so grad(d) = f, grad(f) = d
+
+    # dL / dc = 
+    # L = d * f = (c + e) * f
+    # dL / dc = dL / dd * dd / dc = -2.0 * 1 = -2.0 = dL / de 
+    # d = c + e, so dd/dc = 1 + 0, dd/de = 1.0
+
+    # dL / db = dL / dd * dd / de * de / db
+    # e = b * a, de / db = a, de / da = b
+    # dL / db = -2.0 * 1.0 * 2.0 = -4.0
+    # dL / da = -2.0 * 1.0 * -3.0 = 6.0
